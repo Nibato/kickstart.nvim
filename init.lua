d85fdb20487c7345a5fa8b54dbf00dfc51ff53fc
Nibato <102328+Nibato@users.noreply.github.com>
2025-03-10 16:43:13 -0700

    add bashls

--- init.lua
+++ init.lua
@@ -678,6 +678,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         basedpyright = {},
+        bashls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
